5269d27b2e99bc1e9ce5db01d4261688d756cefc
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-12-04 22:50:41 +0530

    new face detection file added

---
+++ lib/face_detection_home.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class FaceDetectorHome extends StatefulWidget {
+  @override
+  _FaceDetectorHomeState createState() => _FaceDetectorHomeState();
+}
+
+class _FaceDetectorHomeState extends State<FaceDetectorHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("this is the facehome"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,18 @@
 import 'package:flutter/material.dart';
+import 'face_detection_home.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new FaceDetectorApp());
 
-class MyApp extends StatelessWidget {
+class FaceDetectorApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mobile Vison',
-      theme: ThemeData(
-        
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Mobile Vision'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'this is the computer vision app using the ML and AI',
-            ),
-          ],
-        ),
-      ),
-       
+      home: new FaceDetectorHome(),
     );
   }
 }
