bc87dc98ca8d81e2b51eb210e8e6e817ab095190
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-12-05 22:57:07 +0530

    ui for the mobile vision done

--- lib/face_detection_home.dart
+++ lib/face_detection_home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'dart:io';
 
 class FaceDetectorHome extends StatefulWidget {
   @override
@@ -6,10 +7,65 @@ class FaceDetectorHome extends StatefulWidget {
 }
 
 class _FaceDetectorHomeState extends State<FaceDetectorHome> {
+  File image;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("this is the facehome"),
+    return Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Face Detection'),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                buildRowTitle(context, 'Pick Image'),
+                buildSelectImageRowWidget(context)
+              ],
+            )
+        )
+    );
+  }
+
+  
+  Widget buildRowTitle(BuildContext context, String title) {
+    return Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headline,
+          ),
+        )
+    );
+  }
+
+  Widget buildSelectImageRowWidget(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        createButton('Camera'),
+        createButton('Gallery')
+      ],
+    );
+  }
+
+  Widget createButton(String imgSource) {
+    return Expanded(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: RaisedButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              splashColor: Colors.blueGrey,
+              onPressed: () {
+                null;
+              },
+              child: new Text(imgSource)),
+        )
     );
   }
 }
